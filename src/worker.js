95bdcd8750e5b3516259e06b61e92629addde709
duckautomata <duckmechatomata@gmail.com>
2026-03-12 20:50:50 -0500

    Fixed subpath to not redirect

--- src/worker.js
+++ src/worker.js
@@ -15,8 +15,8 @@ export default {
                 return response;
             }
 
-            // It's a navigation route (no extension). Fallback to the SPA's index.html.
-            url.pathname = "/dokinomicon/index.html";
+            // It's a navigation route (no extension). Fallback to the SPA's root.
+            url.pathname = "/dokinomicon/"; 
             return env.ASSETS.fetch(new Request(url, request));
         }
 
